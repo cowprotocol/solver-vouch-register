46de9fb89aff049a55f56710c68f18a0d4bdd96c
josojo <josojo@hotmail.de>
2022-05-03 19:52:51 +0200

    clarify that the full funding amount is needed for the identification of the bonding pool owner

--- src/contracts/VouchRegister.sol
+++ src/contracts/VouchRegister.sol
@@ -19,7 +19,7 @@ contract VouchRegister {
     /// @dev Allows a bonding pool to officially vouch for a solver
     /// Anyone can call this function, but only the events where the sender is the owner
     /// of the referenced bondingPool will be officially indexed
-    /// The owner of a bonding pool is identified by the address sending the initial funding amount to the bonding pool
+    /// The owner of a bonding pool is identified by the address sending the full initial funding to the bonding pool
     /// @param solver The solver for whom the bonding pool will cover potential losses/penalities
     /// @param bondingPool Address of the bonding pool from which a potential loss will be covered
     /// @param cowRewardTarget Address to which the solver COW Token reward should be send for the particular solver
@@ -41,7 +41,7 @@ contract VouchRegister {
     /// @dev Invalidates the vouching for a solver by a bonding pool
     /// Anyone can call this function, but only the events where the sender is the owner
     /// of the referenced bondingPool will be officially indexed
-    /// The owner of a bonding pool is identified by the address sending the initial funding amount to the bonding pool
+    /// The owner of a bonding pool is identified by the address sending the full initial funding amount to the bonding pool
     /// @param solver The solver for whom the bonding pool will no longer cover any losses/penalities
     /// @param bondingPool Address of the official bonding pool, from which a potential loss will no longer be covered
     function invalidateVouching(
